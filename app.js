deac91abb6aa0ec967dcc94b9890e9271b67495f
skyblue16 <arivichi16@gmail.com>
2017-10-05 17:04:12 -0300

    subiendo project CC

---
+++ app.js
@@ -0,0 +1,4 @@
+var person = prompt("Porfavor ingrese una Frase");//Por me dio de prompt, pedimos ingresar una frase
+function Cipher(str,key) {
+//creo la primera funcion Cipher para
+str = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,W,X,Y,Z];
